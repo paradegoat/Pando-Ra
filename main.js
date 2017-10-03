fc9af84aa6929d8dadfb8f0e6728e446dee146a0
Geoff Humphreys <humphreys.geoff@gmail.com>
2017-10-02 23:19:02 -0400

    onkeypress working and style upgrade(incomplete)

--- main.js
+++ main.js
@@ -1,58 +1,62 @@
+console.log("checking in.....HEY HEY");
 
-let searchButton = document.getElementById("search");
 
-searchButton.addEventListener("click", function(event) {
-  let searchField = document.getElementById("searchField").value;
-  let url = 'https://api.soundcloud.com/tracks/?client_id=3f4f6e443227c34b2391dcf6a71ba46a&q=' + (searchField) + '&limit=6';
-  console.log(searchField);
-  var searchBar = document.getElementById("search-bar");
+let trackInput = document.getElementById("trackInput");
+let submitButton = document.getElementById("submitButton");
+submitButton.onclick = searchTracks;
 
-  console.log(searchButton);
-  fetch(url).then(
-    function(response) {
-      if (response.status !==200) {
-        console.log(response.status);
-        alert("this page is broke");
-        return;
-      }
+document.body.addEventListener('keydown', function(e) {
+    if(e.keyCode === 13) {
+      searchTracks();
+    }
+});
 
-      response.json().then(function(data){
-        console.log("here is the data:")
-        console.log(data);
-        console.log(searchField);
-        newData = data;
-        function renderRequest() {
-          let clientId = "/?client_id=3f4f6e443227c34b2391dcf6a71ba46a"
-          return `
+function searchTracks() {
+  let searchResults = trackInput.value;
+    fetch('https://api.soundcloud.com/tracks/?client_id=3f4f6e443227c34b2391dcf6a71ba46a&q=' + searchResults + '&limit=8').then(function(response) {
+        if (response.status != 200) {
+            console.log('Looks like there was a problem. Status Code' + response.status);
+            return;
+        }
+        response.json().then(function(data){
+      console.log("here is the data:")
+      console.log(data);
+      console.log(searchResults);
+          newData = data;
 
-            ${newData.map( element => `
-              <div class="results-box">
-              <button class= "audioTrigger"><img src="${element.artwork_url}" id="${element.stream_url}${clientId}"></button>
-              <ul>
-                <li>${element.title}</li>
-                <li >${element.user.username}</li>
-              </ul>
+          function renderRequest() {
+            let clientId = "/?client_id=3f4f6e443227c34b2391dcf6a71ba46a"
+            return `
+            <div class="bottom-box">
+                <div class= "clearBox">
+                  <h3>Search Results:</h3>
+                </div>
+                </div>
+                <div class="container">
+              ${newData.map( element =>
+                `
+                <div class="results-box">
+                  <button class= "audioTrigger"><img src="${element.artwork_url}" id="${element.stream_url}${clientId}"></button>
+                    <div class="title-element">
+                      ${element.title}
+                    </div>
+                </div>`).join('')
+              }
               </div>
 
-              `).join('')}`;
-        }
+            `
+          }
 
-        let markup =`
-
-        ${renderRequest(data)}
-
-        `
-        document.getElementById("results-box").innerHTML = markup;
-        var parent = document.getElementById('results-box').addEventListener('click', function (event) {
-                  var triggers = document.getElementsByClassName('audioTrigger');
-                  event.target = triggers;
-                  let playTrack = `<audio src="${event.target.id}" id="audio" controls="controls" autoplay></audio>`
-                return document.getElementById('audioWrap').innerHTML = playTrack;
-
-             });
-
-      });
+          let markup =`${renderRequest()}`
+          document.getElementById("results-box").innerHTML = markup;
+            var parent = document.getElementById('results-box').addEventListener('click', function (event) {
+              var triggers = document.getElementsByClassName('audioTrigger');
+              event.target = triggers;
+              let playTrack = `<audio src="${event.target.id}" id="audio" controls="controls" autoplay></audio>`
+                  return document.getElementById('audioWrap').innerHTML = playTrack;
+          });
+        });
 
     }
   );
-})
+}
